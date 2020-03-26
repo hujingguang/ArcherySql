e622d3e1e21847cc2806224a3cf181e7af953a87
Ho_o <hujingguangsa@gmail.com>
2020-03-26 16:30:18 +0800

    Update start.sh

--- run_files/start.sh
+++ run_files/start.sh
@@ -5,3 +5,11 @@ python manage.py makemigrations sql
 python manage.py migrate 
 supervisord -c qcluster_supervisord.conf 
 python manage.py runserver 0.0.0.0:99 --insecure
+
+
+
+
+
+#生产运行命令
+
+#docker run --name yx_archery_2_4_1 -d -v /opt/SQLAdvisor/sqladvisor/sqladvisor:/opt/archery/sqladvisor -v /opt/sql_archery/lib:/opt/venv4archery/lib -v /usr/local/bin/redis-cli:/usr/bin/redis-cli -v /opt/sql_archery/settings.py:/opt/sql_archery/archery/settings.py -p 9123:99 registry.cn-shenzhen.aliyuncs.com/hoover/hoover:sql_archery_v2-4-1
